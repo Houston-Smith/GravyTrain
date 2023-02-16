7800b06f5f3affa537bfd30406b50d77cf984423
Houston-Smith <houstonbsmith@gmail.com>
2023-02-16 10:08:10 -0600

    Added Stars

--- GravyTrain/Client/src/components/review/ReviewCard.js
+++ GravyTrain/Client/src/components/review/ReviewCard.js
@@ -3,14 +3,50 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "./ReviewList.css"
 
+
+
 export const ReviewCard = ({review, callDeleteReview}) => {
 
   const navigate = useNavigate()
 
+  const starCount = (value) => {
+    switch (value) {
+      case 1:
+      return (<p className="score-value"><b className="fa-solid fa-star-half"></b></p>)
+
+      case 2:
+      return (<p className="score-value"><b className="fa-solid fa-star"></b></p>)
+
+      case 3:
+      return (<p className="score-value"><b className="fa-solid fa-star"></b><b className="fa-solid fa-star-half"></b></p>)
+
+      case 4:
+      return (<p className="score-value"><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b></p>)
+
+      case 5:
+      return (<p className="score-value"><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star-half"></b></p>)
+
+      case 6:
+      return (<p className="score-value"><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b></p>)
+
+      case 7:
+      return (<p className="score-value"><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star-half"></b></p>)
+
+      case 8:
+      return (<p className="score-value"><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b></p>)
+
+      case 9:
+      return (<p className="score-value"><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star-half"></b></p>)
+
+      case 10:
+      return (<p className="score-value"><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b><b className="fa-solid fa-star"></b></p>)
+    }
+  }
+
   return (
     <section className="review-card" onClick={() => {navigate(`/review/${review.id}/details`)}}>
             <h2><b>{review.locationName}</b></h2>  
-            <p className="points"><b>{review.averageScore} Points</b></p>                    
+            {starCount(review.averageScore)}                    
             <p>{review.tags.map(tag =>
               <li key={tag.id}>{tag.name}</li>
               )}
