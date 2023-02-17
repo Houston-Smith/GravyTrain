2db348328d897aba3ad00c02a826e10e1fa07939
Houston-Smith <houstonbsmith@gmail.com>
2023-02-17 07:46:46 -0600

    Login Style

--- GravyTrain/Client/src/components/Login.js
+++ GravyTrain/Client/src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { useNavigate, Link } from "react-router-dom";
 import { login } from "../modules/authManager";
+import "./Login.css"
 
 export default function Login() {
   const navigate = useNavigate();
@@ -17,32 +18,100 @@ export default function Login() {
   };
 
   return (
-    <Form onSubmit={loginSubmit}>
-      <fieldset>
-        <FormGroup>
-          <Label for="email">Email</Label>
-          <Input
-            id="email"
-            type="text"
-            autoFocus
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input
-            id="password"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Button>Login</Button>
-        </FormGroup>
-        <em>
-          Not registered? <Link to="/register">Register</Link>
-        </em>
-      </fieldset>
-    </Form>
+    // <Form onSubmit={loginSubmit}>
+    //   <fieldset>
+    //     <FormGroup>
+    //       <Label for="email">Email</Label>
+    //       <Input
+    //         id="email"
+    //         type="text"
+    //         autoFocus
+    //         onChange={(e) => setEmail(e.target.value)}
+    //       />
+    //     </FormGroup>
+    //     <FormGroup>
+    //       <Label for="password">Password</Label>
+    //       <Input
+    //         id="password"
+    //         type="password"
+    //         onChange={(e) => setPassword(e.target.value)}
+    //       />
+    //     </FormGroup>
+    //     <FormGroup>
+    //       <Button>Login</Button>
+    //     </FormGroup>
+    //     <em>
+    //       Not registered? <Link to="/register">Register</Link>
+    //     </em>
+    //   </fieldset>
+
+    //   <fieldset className="LoginFieldset">
+
+    //     <section>
+    //       <b><p className="input-paragraph">Email</p></b> 
+    //       <fieldset className="input-fieldset">
+    //         <label htmlFor="locationName"></label>
+    //         <input  type="text" id="locationName" onChange={(e) => setEmail(e.target.value)} required autoFocus className="form-control"/>
+    //         </fieldset>
+    //     </section>
+
+    //     <section>
+    //       <b><p className="input-paragraph">Password</p></b> 
+    //       <fieldset className="input-fieldset">
+    //         <label htmlFor="locationName"></label>
+    //         <input  type="text" id="locationName" onChange={(e) => setPassword(e.target.value)} required autoFocus className="form-control"/>
+    //         </fieldset>
+    //     </section>
+
+    //     <div className="form-buttons">
+
+    //       <button className="login-button"
+    //         onClick={loginSubmit}>
+    //         Login
+    //       </button>
+          
+    //       {/* <button className="register-button"
+    //         onClick={navigate("/register")}>
+    //         Register
+    //       </button> */}
+
+    //     </div>
+
+
+    //   </fieldset>
+    // </Form>
+
+<fieldset className="LoginFieldset">
+
+<section>
+  <b><p className="input-paragraph">Email</p></b> 
+  <fieldset className="input-fieldset">
+    <label htmlFor="locationName"></label>
+    <input  type="text" id="locationName" onChange={(e) => setEmail(e.target.value)} required autoFocus className="form-control"/>
+    </fieldset>
+</section>
+
+<section>
+  <b><p className="input-paragraph">Password</p></b> 
+  <fieldset className="input-fieldset">
+    <label htmlFor="locationName"></label>
+    <input  type="text" id="locationName" onChange={(e) => setPassword(e.target.value)} required autoFocus className="form-control"/>
+    </fieldset>
+</section>
+
+<div className="form-buttons">
+
+  <button className="login-button"
+    onClick={loginSubmit}>
+    Login
+  </button>
+  
+  <button className="register-button"
+    onClick={() => {navigate(`/register`)}}>Register</button>
+
+</div>
+
+
+</fieldset>
   );
 }
